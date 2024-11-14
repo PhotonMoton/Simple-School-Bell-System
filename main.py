63fbb72b6bf190cfedffb89de8dcf042154eb202
PhotonMoton <rmota29619@gmail.com>
2024-11-14 12:54:59 -0500

    universal server adjust

--- main.py
+++ main.py
@@ -368,4 +368,4 @@ def load_schedule():
 
 # Main block to run the Flask application on a specified host and port
 if __name__ == "__main__":
-    app.run(host='10.0.1.179', port=8080)
+    app.run(host='0.0.0.0', port=8080)
